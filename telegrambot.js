daf24a610bec3af603fc724fd7fa7f0069549f9e
henrymxu <henry.m.xu@live.com>
2018-02-10 12:54:44 -0500

    Added .env file

--- telegrambot.js
+++ telegrambot.js
@@ -1,14 +1,14 @@
 const Telegraf = require('telegraf')
 const express = require('express')
+require('dotenv').config()
 const lyrics = require('./getlyric.js')
 const reddit = require('./getreddit.js')
 const coinmarket = require('./getcoin.js')
 
-const BOT_TOKEN = '546701409:AAFuhCSl0V2G-ZHMCd9vVsaNdhoUBLQ8Aj0'
 app = express()
 app.listen(process.env.PORT || 8080)
-
-const bot = new Telegraf(BOT_TOKEN)
+console.log (process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.start((ctx) => {
   return ctx.reply('Welcome!')
@@ -20,8 +20,8 @@ bot.command('getlyric', (ctx) => {
   var song = ctx.update.message.text.split("/getlyric ")[1]
   Promise.resolve(lyrics.search(song)).then(function(values) {
     splitValues = chunkString(values, 4096);
-    for (var i = 0; i < splitValues.length; i++) {
-      ctx.reply(splitValues[i]);
+    for (var i = splitValues.length - 1; i >= 0; i--) {
+      ctx.replyWithMarkdown('`' + splitValues[i] + '`');
     }
   })
 })
@@ -50,27 +50,28 @@ bot.command('listcoins', (ctx) => {
     ctx.reply('Value: -69$, fuck you');
   })
 })
-
-bot.hears('jimmy', (ctx) => ctx.reply('Where\'s my food Jimmy'))
-
 bot.on('sticker', (ctx) => ctx.reply('👍'))
 
 bot.on('message', ctx => {
+  if (ctx.from.first_name == 'Jimmy' && (ctx.update.message.text == 'wtf' || ctx.update.message.text == 'wat')) {
+    ctx.reply('Shut the fuck up Jimmy')
+  }
   if (ctx.from.first_name == 'Jimmy') {
     ctx.reply('Where\'s my food Jimmy')
+    return;
   }
 })
 
 bot.startPolling()
 
 function chunkString(str, len) {
-  var _size = Math.ceil(str.length/len),
-      _ret  = new Array(_size),
-      _offset
-  ;
-  for (var _i=0; _i<_size; _i++) {
-    _offset = _i * len;
-    _ret[_i] = str.substring(_offset, _offset + len);
+  var counter = 0;
+  var result = [];
+  while (str.length > 4000) {
+    result[counter] = str.substring(0, 4000);
+    str = str.replace(result[counter], '')
+    counter++;
   }
-  return _ret;
+  result[counter] = str;
+  return result;
 }
